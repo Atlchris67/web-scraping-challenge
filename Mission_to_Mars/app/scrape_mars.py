586ba74414c5706637d51e1a2ece03e331ab8627
Atlchris67 <atlchris@gmail.com>
2020-03-02 19:55:35 -0500

    check in ugly code that works

--- Mission_to_Mars/app/scrape_mars.py
+++ Mission_to_Mars/app/scrape_mars.py
@@ -7,7 +7,7 @@ import time
 def scrape_all():
 
     # Initiate headless driver for deployment
-    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    browser = Browser("chrome", executable_path="../chromedriver", headless=True)
     news_title, news_paragraph = mars_news(browser)
 
     # Run all scraping functions and store in dictionary.
@@ -16,9 +16,9 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "hemispheres": hemispheres(browser),
-        "weather": twitter_weather(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "weather": twitter_weather(),
+         "facts": mars_facts(),
+         "last_modified": dt.datetime.now()
     }
 
     # Stop webdriver and return data
@@ -29,15 +29,28 @@ def scrape_all():
 def mars_news(browser):
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
-
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    news_titles = {} 
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
     #write your code here
+    news_title = soup.find('div', class_='content_title').text
+    news_p = soup.find('div', class_='article_teaser_body').text
     return news_title, news_p
 
 
 def featured_image(browser):
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url)
-    #write your code here
+    browser.is_element_present_by_css("article.carousel_item", wait_time=1)
+    browser.click_link_by_id('full_image')
+    browser.click_link_by_partial_text('more info')
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    feat_img_url = soup.find('figure', class_='lede').a['href']
+    main_url = "https://www.jpl.nasa.gov"
+    img_url = main_url + feat_img_url
     return img_url
 
 
@@ -50,22 +63,57 @@ def hemispheres(browser):
     )
 
     browser.visit(url)
-    #write your code here
-    return hemisphere_image_urls
+    html = browser.html
+    soup = BeautifulSoup(html , 'html.parser')
+    hemisphere_info = []
+    hemis = soup.find_all('div', class_="item")
+    hemi_fqdn = 'https://astrogeology.usgs.gov'
+
+    for sphere in hemis:
+        title = sphere.find('h3').text
+        image_url = sphere.find('a', class_='itemLink product-item')['href']
+        browser.visit(hemi_fqdn  + image_url)
+        image_html = browser.html
+        soup = BeautifulSoup( image_html, 'html.parser')
+        image_url = hemi_fqdn + soup.find('img', class_='wide-image')['src']
+        hemisphere_info.append({"title" : title.replace('Enhanced',""), "img_url" : image_url})
 
 
-def twitter_weather(browser):
-    url = "https://twitter.com/marswxreport?lang=en"
-    browser.visit(url)
+    return hemisphere_info
+
+
+def twitter_weather():
+
+    browser2 = Browser("chrome", executable_path="../chromedriver", headless=True)
+    url = 'https://twitter.com/marswxreport?lang=en'
+    browser2.visit(url)
     #write your code here
+    time.sleep(5)
+    html = browser2.html
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    weather_divs = soup.find_all('div', class_="css-901oao r-hkyrab r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0")
+    mars_weather = "not found"
+    for weather in weather_divs:
+    #     print(tweet)
+        mars_weather = weather.text
+        if 'sol' and 'pressure' in mars_weather:
+            break
+        else:
+            pass
+
+    browser2.quit()
+
     return mars_weather
 
 
 
 
 def mars_facts():
+    url = "https://space-facts.com/mars/"
     try:
-        df = pd.read_html("http://space-facts.com/mars/")[0]
+        df = pd.read_html(url)[0]
     except BaseException:
         return None
 
